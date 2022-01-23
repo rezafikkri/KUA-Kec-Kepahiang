6b8cc52ae688874f439c23e3c5309deec783a5b6
fikkrireza <fikkri.reza@gmail.com>
2022-01-23 08:11:45 +0700

    build: config webpack

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import regeneratorRuntime from 'regenerator-runtime';
+
+// bootstrap plugin
+import { Collapse } from 'bootstrap';
+
+// assets
+import './font/Work_Sans/OFL.txt';
+
+// css
+import './css/app.css';
---
+++ webpack.common.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'public/dist'),
+        filename: 'js/app.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(woff|woff2|eot|ttf|otf|svg|txt)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'font/Nunito/[base]'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'css/app.bundle.css'
+        })
+    ]
+};
