efc824a7cf0e60e3d4778ee2ddf56b182a9af4af
fikkrireza <fikkri.reza@gmail.com>
2022-02-20 22:01:08 +0700

    refactor: user try and catch for handling error in get photo

--- public/js/foto.js
+++ public/js/foto.js
@@ -4,55 +4,61 @@ let msnry;
 
 async function showInstagramImages(galeriElement, galeriGridElement)
 {
-    const response = await fetch(`foto/gets?limit=20`);
-    const responseJson = await response.json();
+    try {
+        const response = await fetch(`foto/gets?limit=20`);
+        const responseJson = await response.json();
     
-    let elems = '<div class="galeri-sizer"></div>';
-    responseJson.images.forEach((img) => {
-        elems += `
-            <div class="galeri-item">
-                <img src="${img.media_url}" alt="${img.caption}">
-                <div class="detail-galeri position-absolute p-3">
-                    <h5>${img.caption}</h5>
+        let elems = '<div class="galeri-sizer"></div>';
+        responseJson.images.forEach((img) => {
+            elems += `
+                <div class="galeri-item">
+                    <img src="${img.media_url}" alt="${img.caption}">
+                    <div class="detail-galeri position-absolute p-3">
+                        <h5>${img.caption}</h5>
+                    </div>
                 </div>
-            </div>
-        `;
-    });
+            `;
+        });
 
-    // inner html images to galeri grid element
-    galeriGridElement.innerHTML = elems;
+        // inner html images to galeri grid element
+        galeriGridElement.innerHTML = elems;
 
-    msnry = new Masonry(galeriGridElement, {
-        itemSelector: '.galeri-item',
-        columnWidth: '.galeri-sizer',
-        percentPosition: true
-    });
+        msnry = new Masonry(galeriGridElement, {
+            itemSelector: '.galeri-item',
+            columnWidth: '.galeri-sizer',
+            percentPosition: true
+        });
 
-    imagesLoaded(galeriGridElement, () => {
-        // layout Masonry after each image loads
-        msnry.layout();
-        
-        // show btn show-next-images
-        const conBtnLoadingElement = document.createElement('div');
-        conBtnLoadingElement.classList.add('btn-loading');
-        
-        let bodyBtnLoadingElement = '';
-        if (responseJson.nextPageUrl) {
-            bodyBtnLoadingElement += `<a href="#" class="btn btn-green" data-next-page-url="${responseJson.nextPageUrl}" id="show-next-images">`;
-        } else {
-            bodyBtnLoadingElement += '<a href="#" class="btn btn-green disabled" id="show-next-images">';
+        imagesLoaded(galeriGridElement, () => {
+            // layout Masonry after each image loads
+            msnry.layout();
+            
+            // show btn show-next-images
+            const conBtnLoadingElement = document.createElement('div');
+            conBtnLoadingElement.classList.add('btn-loading');
+            
+            let bodyBtnLoadingElement = '';
+            if (responseJson.nextPageUrl) {
+                bodyBtnLoadingElement += `<a href="#" class="btn btn-green" data-next-page-url="${responseJson.nextPageUrl}" id="show-next-images">`;
+            } else {
+                bodyBtnLoadingElement += '<a href="#" class="btn btn-green disabled" id="show-next-images">';
 
-            // show message
-            const messageElement = document.createElement('p');
-            messageElement.classList.add('text-muted');
-            messageElement.innerText = 'Foto sudah habis.';
-            galeriElement.appendChild(messageElement);
-        }
+                // show message
+                const messageElement = document.createElement('p');
+                messageElement.classList.add('text-muted');
+                messageElement.innerText = 'Foto sudah habis.';
+                galeriElement.appendChild(messageElement);
+            }
 
-        bodyBtnLoadingElement += 'Lihat Lebih Banyak Foto</a><div class="loading d-none" id="loading"><div></div></div>';
-        conBtnLoadingElement.innerHTML = bodyBtnLoadingElement;
-        galeriElement.appendChild(conBtnLoadingElement);
-    });
+            bodyBtnLoadingElement += 'Lihat Lebih Banyak Foto</a><div class="loading d-none" id="loading"><div></div></div>';
+            conBtnLoadingElement.innerHTML = bodyBtnLoadingElement;
+            galeriElement.appendChild(conBtnLoadingElement);
+        });
+    } catch (error) {
+        // remove loading
+        document.querySelector('#loading').remove();
+        console.error(error);
+    }
 }
 
 showInstagramImages(galeriElement, galeriGridElement);
@@ -67,47 +73,54 @@ galeriElement.addEventListener('click', async (e) => {
         loadingElement.classList.remove('d-none');
 
         const nextPageUrl = encodeURIComponent(e.target.dataset.nextPageUrl);
-        const response = await fetch(`foto/gets?limit=20&nextPageUrl=${nextPageUrl}`);
-        const responseJson = await response.json();
 
-        let elems = [];
-        let fragment = document.createDocumentFragment();
-        responseJson.images.forEach((img) => {
-            const galeriItemElement = document.createElement('div');
-            galeriItemElement.classList.add('galeri-item');
-            galeriItemElement.innerHTML = `
-                <img src="${img.media_url}" alt="${img.caption}">
-                <div class="detail-galeri position-absolute p-3">
-                    <h5>${img.caption}</h5>
-                </div>
-            `;
+        try {
+            const response = await fetch(`foto/gets?limit=20&nextPageUrl=${nextPageUrl}`);
+            const responseJson = await response.json();
 
-            fragment.appendChild(galeriItemElement);
-            elems.push(galeriItemElement);
-        });
+            let elems = [];
+            let fragment = document.createDocumentFragment();
+            responseJson.images.forEach((img) => {
+                const galeriItemElement = document.createElement('div');
+                galeriItemElement.classList.add('galeri-item');
+                galeriItemElement.innerHTML = `
+                    <img src="${img.media_url}" alt="${img.caption}">
+                    <div class="detail-galeri position-absolute p-3">
+                        <h5>${img.caption}</h5>
+                    </div>
+                `;
 
-        // append element to galeri grid
-        galeriGridElement.appendChild(fragment);
-        imagesLoaded(galeriGridElement, () => {
-            // add and lay out newly appended elements
-            msnry.appended(elems);
+                fragment.appendChild(galeriItemElement);
+                elems.push(galeriItemElement);
+            });
 
-            // if next page url not undefined
-            if (responseJson.nextPageUrl) {
-                // replace next page url in button
-                e.target.dataset.nextPageUrl = responseJson.nextPageUrl;
-            } else {
-                // disabled button show next images
-                e.target.classList.add('disabled');
-                // show message
-                const messageElement = document.createElement('p');
-                messageElement.classList.add('text-muted');
-                messageElement.innerText = 'Foto sudah habis.';
-                e.target.parentElement.parentElement.insertBefore(messageElement, e.target.parentElement);
-            }
+            // append element to galeri grid
+            galeriGridElement.appendChild(fragment);
+            imagesLoaded(galeriGridElement, () => {
+                // add and lay out newly appended elements
+                msnry.appended(elems);
 
+                // if next page url not undefined
+                if (responseJson.nextPageUrl) {
+                    // replace next page url in button
+                    e.target.dataset.nextPageUrl = responseJson.nextPageUrl;
+                } else {
+                    // disabled button show next images
+                    e.target.classList.add('disabled');
+                    // show message
+                    const messageElement = document.createElement('p');
+                    messageElement.classList.add('text-muted');
+                    messageElement.innerText = 'Foto sudah habis.';
+                    e.target.parentElement.parentElement.insertBefore(messageElement, e.target.parentElement);
+                }
+
+                // hide loading
+                loadingElement.classList.add('d-none');
+            });
+        } catch (error) {
             // hide loading
             loadingElement.classList.add('d-none');
-        });
+            console.error(error);
+        }
     }
 });
--- public/js/video.js
+++ public/js/video.js
@@ -41,6 +41,8 @@ async function showYoutubeVideos(galeriElement, galeriGridElement)
         conBtnLoadingElement.innerHTML = bodyBtnLoadingElement;
         galeriElement.appendChild(conBtnLoadingElement);
     } catch (error) {
+        // remove loading
+        document.querySelector('#loading').remove();
         console.error(error);
     }
 }
